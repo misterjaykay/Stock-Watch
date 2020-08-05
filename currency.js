eadcd4c4eaad5cace7adf6348214c6b5b018105c
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-05 14:30:10 -0400

    changed the currency values to 2 decimal places

--- currency.js
+++ currency.js
@@ -15,26 +15,31 @@ $.ajax({
 
     // get the exchange rate in euros
     var euroRate = response.rates.EUR;
+    euroRate = euroRate.toFixed(2);
     // passes the value stored inside the variable into an id
     $("#euroRate").text(euroRate);
 
     // get exchange rate for yen
     var jpyRate = response.rates.JPY;
+    jpyRate = jpyRate.toFixed(2);
     // passes the value stored inside the variable into an id
     $("#jpyRate").text(jpyRate);
 
     // get the exchange rate for great british pound
     var gbpRate = response.rates.GBP;
+    gbpRate = gbpRate.toFixed(2);
     // passes the value stored inside the variable into an id
     $("#gbpRate").text(gbpRate);
 
     // get the exchange rate for canadian dollar
     var cadRate = response.rates.CAD;
+    cadRate = cadRate.toFixed(2);
     // passes the value stored inside the variable into an id
     $("#cadRate").text(cadRate);
 
     // get the exchange rate for kuwaiti dinar
     var kwdRate = response.rates.KWD;
+    kwdRate = kwdRate.toFixed(2);
     // passes the value stored inside the variable into an id
     $("#kwdRate").text(kwdRate);
 });
