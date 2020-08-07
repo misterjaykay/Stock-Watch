5d7147e19849dab7fd71caa566daceb88f3c14ea
Georgios Mpovolis <gam154@scarletmail.rutgers.edu>
2020-08-06 20:29:12 -0400

    changed everything to use local storage, still some bugs

--- iex.js
+++ iex.js
@@ -1,10 +1,21 @@
 var baseURLIEX = "https://cloud.iexapis.com/stable/stock/"
 var apiToken = "?token=pk_85904d0710804cf3865bcf30040ebec9"
-var index = 0;
 var quoteURL = "/quote";
 var newsURLBase = "/news/last/1";
 var intraDayURLBase = "/intraday-prices";
 var stockListURL = "https://financialmodelingprep.com/api/v3/company/stock/list?apikey=50cb9e8f707557d71a23370f6431ed64";
+var baseObject = {"name": "", "symbol":"", "latestPrice":"", "high":"", "low":"", "open":"", "close":"", "articleURL": "", "articleHeadline": ""};
+localStorage.setItem("baseObject", JSON.stringify(baseObject))
+
+$(".search-btn-name").on("click", function(event){
+    event.preventDefault();
+    $(".user-input-symbol").attr("value", "");
+}) //empties the symbol input so it wont conflict with name search
+
+$(".search-btn-symbol").on("click", function(event){
+    event.preventDefault();
+    $(".user-input-name").attr("value", "");
+}) // empties the name input so it wont conflict with symbol search
 
 $(".search-btn").on("click", function(event) {
     event.preventDefault();
@@ -16,50 +27,122 @@ $(".search-btn").on("click", function(event) {
        for(var i = 0; i<response4.symbolsList.length; i++){
             var name = response4.symbolsList[i].name;
             var symbol = response4.symbolsList[i].symbol;
-            if($(".user-input-name").val().trim().toLowerCase()==name.toLowerCase() && $(".user-input-name").val()!==""){
-                var storageArr = [""];
-                if(localStorage.getItem("companySymbol")!==null){
-                    storageArr = JSON.parse(localStorage.getItem("companySymbol"));
-                    console.log(storageArr);  
+            // assigns variables for current stock name and symbol it is comparing to user input
+            
+            if($(".user-input-name").val().trim().toLowerCase()==name.toLowerCase() && $(".user-input-name").val() != ""){
+                // finds a match for the name of the stock also checks if input is empty
+                
+                localStorage.setItem("matchedName", name);
+                localStorage.setItem("matchedSymbol", symbol);
+                // saves matched info to local storage to pull later out of scope of variables
+                
+                var storageArr = [];
+                //initializes a storage arr to hold each stockObject
+                
+                if(JSON.parse(localStorage.getItem("stockObjects")) != null || JSON.parse(localStorage.getItem("stockObjects")) != ""){
+                    //checks if local storage has info already
+
+                    for(var i = 0; i < JSON.parse(localStorage.getItem("stockObjects")).length; i++){
+                    storageArr.push(JSON.parse(localStorage.getItem("stockObjects"))[i]);
+                    console.log(storageArr);
+                    //if there is a search history in local storage it pushes all elements into an array
+                    }
+                    storageArr.push(JSON.parse(localStorage.getItem("baseObject")));
+                    // adds an object template to the end of the array of stockObjects
+
+                    storageArr[storageArr.length-1].name = localStorage.getItem(matchedName);
+                    storageArr[storageArr.length-1].symbol = localStorage.getItem(matchedSymbol);
+                    // adds stock name and symbol to that stockObject template
+
+                    localStorage.setItem("stockObjects", storageArr);
+                    // update the saved stockObjects
+                
+                } else {
+                    storageArr.push(JSON.parse(localStorage.getItem("baseObject")));
+                    // adds an stockObject template to the empty array
+                    
+                    storageArr[storageArr.length-1].name = localStorage.getItem(matchedName);
+                    storageArr[storageArr.length-1].symbol = localStorage.getItem(matchedSymbol);
+                    // adds stock name and symbol to the stockObject template 
                 }
-                if(storageArr[0]===""){
-                    console.log("pop")
-                    storageArr.pop();
-                }
-                console.log("symbol: " + symbol);
-                storageArr.push(symbol);
-                console.log("storageArr: "+ storageArr + ";");
-                localStorage.setItem("companySymbol", JSON.stringify(storageArr));
-                localStorage.setItem("latestSymbol", storageArr[storageArr.length-1]);
+
+                localStorage.setItem("latestSymbol", JSON.parse(localStorage.getItem("stockObjects"))[(JSON.parse(localStorage.getItem("stockObjects")).length-1)].symbol);
+                //saves the latest symbol
                 break;
-            } else if($(".user-input-symbol").val().trim().toLowerCase()==symbol.toLowerCase() && $(".user-input-symbol").val()!==""){
-                if(localStorage.getItem("companySymbol")===null){
-                    storageArr = [$(".user-input-symbol").val()];  
-                  }
-                    storageArr.push(symbol)
-                    localStorage.setItem("companySymbol", JSON.stringify(storageArr));
-                    localStorage.setItem("latestSymbol", storageArr[storageArr.length-1]);
-                    break;
+            } else if($(".user-input-symbol").val().trim().toLowerCase()==symbol.toLowerCase() && $(".user-input-symbol").val() != ""){
+                localStorage.setItem("matchedName", name);
+                localStorage.setItem("matchedSymbol", symbol);
+                // saves matched info to local storage to pull later out of scope of variables
+                
+                var storageArr = [];
+                //initializes a storage arr to hold each stockObject
+                
+                if(localStorage.getItem("stockObjects") != null || localStorage.getItem("stockObjects") != ""){
+                    //checks if local storage has info already
+
+                    for(var i = 0; i < JSON.parse(localStorage.getItem("stockObjects")).length; i++){
+                    storageArr.push(JSON.parse(localStorage.getItem("stockObjects"))[i]);
+                    console.log(storageArr);
+                    //if there is a search history in local storage it pushes all elements into an array
+                    }
+                    storageArr.push(JSON.parse(localStorage.getItem("baseObject")));
+                    // adds an object template to the end of the array of stockObjects
+
+                    storageArr[storageArr.length-1].name = localStorage.getItem(matchedName);
+                    storageArr[storageArr.length-1].symbol = localStorage.getItem(matchedSymbol);
+                    // adds stock name and symbol to that stockObject template
+
+                    localStorage.setItem("stockObjects", storageArr);
+                    // update the saved stockObjects
+                
+                } else {
+                    storageArr.push(JSON.parse(localStorage.getItem("baseObject")));
+                    // adds an stockObject template to the empty array
+                    
+                    storageArr[storageArr.length-1].name = localStorage.getItem(matchedName);
+                    storageArr[storageArr.length-1].symbol = localStorage.getItem(matchedSymbol);
+                    // adds stock name and symbol to the stockObject template 
+                }
+
+                localStorage.setItem("latestSymbol", JSON.parse(localStorage.getItem("stockObjects"))[(JSON.parse(localStorage.getItem("stockObjects")).length-1)].symbol);
+                //saves the latest symbol
+                break;
             } else{
                 //add modal alert that no match for search;
             }
         }
     
-    // Builds both URLs
-    buildiexURLs();
     
+    buildiexURLs(); // Builds all URLs using the saved latestSymbol
+   
+
     //stock ajax for after market closes
     $.ajax({
       url: iexURL,
       method: "GET"
     })
     .then(function(response){
-        var compName = response.companyName;
-        var latestPrice = response.latestPrice;
-        var high = response.high;
-        var low = response.low;
-        var open = response.open;
-        var close = response.close;
+        //response holds the info for the latest symbol
+
+        baseObject.name =response.companyName;
+        baseObject.latestPrice = response.latestPrice;
+        baseObject.high = response.high;
+        baseObject.low = response.low;
+        baseObject.open = response.open;
+        baseObject.close = response.close;
+        // uses the stockObject template to hold all values before they get saved
+
+        var localArr = JSON.parse(localStorage.getItem("stockObjects"))
+        // initialize a local array to hold the array of stock objects
+
+        localArr[localArr.length-1] = baseObject;
+        //replaces the last stock object in the array with one that has all the info gathered so far
+
+        localStorage.setItem("stockObjects", localArr);
+        // Updates the saved array of stock objects
+
+        baseObject = {"name": "", "symbol":"", "latestPrice":"", "high":"", "low":"", "open":"", "close":"", "articleURL": "", "articleHeadline": ""};
+        //returns baseObject to its former glory
         
     // intraDay stock info
     $.ajax({
@@ -72,32 +155,32 @@ $(".search-btn").on("click", function(event) {
                 break;
             }
             responseIndex--;
+        } 
+        //while loop searches intraDayUrl to find latest non null info and establishes an index to that array object with proper info
+
+        if(JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].high===null){
+            JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].high = response2[responseIndex].high;
+            JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].low = response2[responseIndex].low;
+            JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].open = response2[responseIndex].open;
+            JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].close = response2[responseIndex].close;
         }
-        if(high===null){
-            high = response2[responseIndex].high;
-            low = response2[responseIndex].low;
-            open = response2[responseIndex].open;
-            close = response2[responseIndex].close;
-        }
+        // if statment check if data pulled from IEX quote URL is null, if it is then it uses new data from IEX intraDay URL to replace the null data
     
-        //Latest Article ajax
+        //AJAX call to receive the latest article corresponding with the current stock being worked with
         $.ajax({
             url: newsURL,
             method: "GET"
         }).then(function(response1){
-            var headline= response1[0].headline;
-            // var source = response1[0].source;
-            // source for article if needed 
-            var articleURL = response1[0].url;
+            JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].articleHeadline= response1[0].headline;
+            JSON.parse(localStorage.getItem("stockObjects"))[JSON.parse(localStorage.getItem("stockObjects")).length-1].articleURL = response1[0].url;
+            // Adds the article headline and URL into the latest stock object
         
         displayStock();
         function displayStock(){
             $(".stock-displayed").detach();
-            for(var i = 0; i < JSON.parse(localStorage.getItem("companySymbol")).length; i++){
-            $(".stock-table").prepend('<tr class="stock-displayed stock-displayed'+i+'"><td>'+compName+'</td><td>$'+latestPrice.toFixed(2)+'</td><td>$'+high.toFixed(2)+'</td><td>$'+low.toFixed(2)+'</td><td>$'+open.toFixed(2)+'</td><td>$'+close.toFixed(2)+'</td><td><a href="'+ articleURL +'">'+headline+'</a></td><td><button class="clear-btn clear-btn'+i+'">Remove</button></td></tr>');
-            //creates a table row for the stock searched and adds all info at once
-             index++
-            }
+            for(var i = 0; i < JSON.parse(localStorage.getItem("stockObjects")).length; i++){
+            $(".stock-table").prepend('<tr class="stock-displayed stock-displayed'+i+'"><td>'+JSON.parse(localStorage.getItem("stockObjects"))[i].name+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].latestPrice+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].high+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].low+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].open+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].close+'</td><td><a href="'+ JSON.parse(localStorage.getItem("stockObjects"))[i].articleURL +'">'+JSON.parse(localStorage.getItem("stockObjects"))[i].headline+'</a></td><td><button class="clear-btn clear-btn'+i+'">Remove</button></td></tr>');
+            } //creates a table row for the stock searched and adds all info at once from local storage
 
             $(".table-heading").remove();
             // removes previous table heading
@@ -113,7 +196,7 @@ $(".search-btn").on("click", function(event) {
             var loopIndex = 20;
             var removeIndex ="";
             //initializes the removeIndex
-            while(JSON.stringify(this.getAttribute("class")).charAt(loopIndex)!=='"'){
+            while(JSON.stringify(this.getAttribute("class")).charAt(loopIndex) != '"'){
                 removeIndex += JSON.stringify(this.getAttribute("class")).charAt(loopIndex);
                 loopIndex++;
             }
@@ -126,44 +209,37 @@ $(".search-btn").on("click", function(event) {
     })
 })
 
-console.log(localStorage.getItem("companySymbol"));
-console.log(JSON.stringify(localStorage.getItem("companySymbol")));
-console.log(JSON.parse(localStorage.getItem("companySymbol")));
+console.log(localStorage.getItem("stockObjects"));
+console.log(JSON.parse(localStorage.getItem("stockObjects")));
 
 $(".clear-btn-all").on("click", function(event){
     event.preventDefault();
 
     $(".stock-displayed").detach();
-    localStorage.removeItem("companySymbol");
+    localStorage.removeItem("stockObjects");
     localStorage.removeItem("latestSymbol");
     console.log("button Works");
 })
 
 function buildiexURLs(){
-    // var symbolInput = $(".user-input-symbol").val().trim();
-    // if(symbolInput==""){
-    //     symbolInput = localStorage.getItem("companySymbol");
-    // }
-    // console.log(symbolInput);
     iexURL = baseURLIEX + localStorage.getItem("latestSymbol") + quoteURL + apiToken;
     newsURL = baseURLIEX + localStorage.getItem("latestSymbol") + newsURLBase + apiToken;
     intraDayURL = baseURLIEX + localStorage.getItem("latestSymbol") + intraDayURLBase + apiToken;
 }
 
 function displayHistory() {
+    if(JSON.parse(localStorage.getItem("stockObjects")) != null || JSON.parse(localStorage.getItem("stockObjects")) != ""){
     $(".stock-displayed").detach();
-    for(var i = 0; i < JSON.parse(localStorage.getItem("companySymbol")).length; i++){
-        $(".stock-table").prepend('<tr class="stock-displayed stock-displayed'+index+'"><td>'+compName+'</td><td>$'+latestPrice.toFixed(2)+'</td><td>$'+high.toFixed(2)+'</td><td>$'+low.toFixed(2)+'</td><td>$'+open.toFixed(2)+'</td><td>$'+close.toFixed(2)+'</td><td><a href="'+ articleURL +'">'+headline+'</a></td><td><button class="clear-btn clear-btn'+index+'">Remove</button></td></tr>');
-        //creates a table row for the stock searched and adds all info at once
-        index++
-    }
+    for(var i = 0; i < JSON.parse(localStorage.getItem("stockObjects")).length; i++){
+        $(".stock-table").prepend('<tr class="stock-displayed stock-displayed'+i+'"><td>'+JSON.parse(localStorage.getItem("stockObjects"))[i].name+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].latestPrice+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].high+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].low+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].open+'</td><td>$'+JSON.parse(localStorage.getItem("stockObjects"))[i].close+'</td><td><a href="'+ JSON.parse(localStorage.getItem("stockObjects"))[i].articleURL +'">'+JSON.parse(localStorage.getItem("stockObjects"))[i].headline+'</a></td><td><button class="clear-btn clear-btn'+i+'">Remove</button></td></tr>');
+        }  //creates a table row for each of the stocks searched and adds all info for each row at once from local storage
 
     $(".table-heading").remove();
     // removes previous table heading
 
     $(".stock-table").prepend('<tr class="table-heading"><th>Company</th><th>Latest Price</th><th>Daily High</th><th>Daily Low</th><th>Opening Price</th><th>Closing Price</th><th>Latest Article:</th><th></th></tr>');
-    //create the table heading dynamically
-}
+    } //create the table heading dynamically
+} // Pulls all the search history for each stock saved in stockObjects and creates a table based off the info
 
 function renderTopStock() {
     var topStockList = ["AAPL", "TSLA", "AMZN", "MSFT", "NFLX", "NVDA" ];
@@ -199,7 +275,10 @@ function renderTopStock() {
 }
 
 renderTopStock();
-// displayHistory();
+if(localStorage.getItem("stockObjects") != null){
+displayHistory();
+}
+
 
 // working iexURL
 // https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_85904d0710804cf3865bcf30040ebec9
